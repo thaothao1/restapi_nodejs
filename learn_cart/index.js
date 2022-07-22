d6e01a611aab03fb6110e99ee5a6fe2f25146707
OS! <thaovuthikimthao@gmail.com>
2022-07-22 17:41:31 +0700

    add code

---
+++ learn_cart/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DBConnection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/user", userRoute);
+app.use("/api/auth/", authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
